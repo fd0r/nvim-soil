00ffcb94a2a005b82aeaef030352a52d1ec10581
Luis Montero <luismontero@hotmail.fr>
2025-01-14 12:01:04 +0100

    feat: add an option to pass a callback to set the output file
    In some situations you want the output file to be in another location
    than the one containing the source plantuml file.
    
    By adding a modifiable callback in the configuration of the plugin one
    can easily change the target file.

--- lua/soil.lua
+++ lua/soil.lua
@@ -1,56 +1,61 @@
-local Logger = require 'soil.logger':new("Soil")
+local Logger = require('soil.logger'):new 'Soil'
 local M = {}
 
 M.DEFAULTS = {
-    actions = {
-        redraw = false
-    },
-    image = {
-        darkmode = false,
-        format = "png",
-        execute_to_open = function(img)
-            return "nsxiv -b " .. img
-        end
-    }
+  actions = {
+    redraw = false,
+  },
+
+  image = {
+    darkmode = false,
+    format = 'png',
+    execute_to_open = function(img)
+      return 'nsxiv -b ' .. img
+    end,
+    plantuml_file_to_output_file = function(file, settings)
+      local out_file = string.format('out/%s.%s', file, settings.image.format)
+      return out_file
+    end,
+  },
 }
 
 function M.setup(opts)
-    if opts.puml_jar then
-        M.DEFAULTS.puml_jar = opts.puml_jar
+  if opts.puml_jar then
+    M.DEFAULTS.puml_jar = opts.puml_jar
+  end
+  if opts.actions then
+    local actions = opts.actions
+    if actions.redraw ~= nil and type(actions.redraw) == 'boolean' and actions.redraw then
+      M.DEFAULTS.actions.redraw = true
     end
-    if opts.actions then
-        local actions = opts.actions
-        if actions.redraw ~= nil and type(actions.redraw) == "boolean" and actions.redraw then
-            M.DEFAULTS.actions.redraw = true
-        end
+  end
+  if opts.image then
+    local img = opts.image
+
+    if img.format then
+      if img.format == 'png' or img.format == 'svg' then
+        M.DEFAULTS.image.format = img.format
+      else
+        Logger:error "Setup Error: The values allowed for image.format are 'png' or 'svg'."
+      end
     end
-    if opts.image then
-        local img = opts.image
 
-        if img.format then
-            if img.format == "png" or img.format == "svg" then
-                M.DEFAULTS.image.format = img.format
-            else
-                Logger:error("Setup Error: The values allowed for image.format are 'png' or 'svg'.")
-            end
-        end
-
-        if img.darkmode then
-            if type(img.darkmode) == "boolean" then
-                M.DEFAULTS.image.darkmode = img.darkmode
-            else
-                Logger:error("Setup Error: image.darkmode must be a boolean value.")
-            end
-        end
-
-        if img.execute_to_open then
-            if type(img.execute_to_open) == "function" then
-                M.DEFAULTS.image.execute_to_open = img.execute_to_open
-            else
-                Logger:error("Setup Error: image.execute_to_open must be a function.")
-            end
-        end
+    if img.darkmode then
+      if type(img.darkmode) == 'boolean' then
+        M.DEFAULTS.image.darkmode = img.darkmode
+      else
+        Logger:error 'Setup Error: image.darkmode must be a boolean value.'
+      end
     end
+
+    if img.execute_to_open then
+      if type(img.execute_to_open) == 'function' then
+        M.DEFAULTS.image.execute_to_open = img.execute_to_open
+      else
+        Logger:error 'Setup Error: image.execute_to_open must be a function.'
+      end
+    end
+  end
 end
 
 return M
--- lua/soil/core.lua
+++ lua/soil/core.lua
@@ -1,88 +1,94 @@
-local Logger = require 'soil.logger':new("Soil")
-local settings = require 'soil'.DEFAULTS
+local Logger = require('soil.logger'):new 'Soil'
+local settings = require('soil').DEFAULTS
 local M = {}
 
 local function validate()
-    local function validate_image_function()
-        return string.find(settings.image.execute_to_open(""), "nsxiv")
-    end
+  local function validate_image_function()
+    return string.find(settings.image.execute_to_open '', 'nsxiv')
+  end
 
-    if vim.bo.filetype ~= "plantuml" then
-        Logger:warn("This is not a Plant UML file.")
-        return false
-    end
-    if vim.fn.executable("java") == 0 then
-        Logger:warn("java is required. Install it to use this plugin.")
-        return false
-    end
-    if vim.fn.executable("nsxiv") == 0 and validate_image_function() then
-        Logger:warn("nsxiv is required. Install it to use this plugin.")
-        return false
-    end
-    return true
+  if vim.bo.filetype ~= 'plantuml' then
+    Logger:warn 'This is not a Plant UML file.'
+    return false
+  end
+  if vim.fn.executable 'java' == 0 then
+    Logger:warn 'java is required. Install it to use this plugin.'
+    return false
+  end
+  if vim.fn.executable 'nsxiv' == 0 and validate_image_function() then
+    Logger:warn 'nsxiv is required. Install it to use this plugin.'
+    return false
+  end
+  return true
 end
 
 local function get_image_command(file)
-    vim.cmd("redraw")
-    local image_file = string.format("%s.%s", file, settings.image.format)
-    if image_file == nil then
-        do return end
+  vim.cmd 'redraw'
+  local image_file = settings.image.plantuml_file_to_output_file(file, settings)
+  if image_file == nil then
+    do
+      return
     end
-    Logger:info(string.format("Image %s.%s generated!", file, settings.image.format))
-    return string.format("sh -c '%s & disown; echo $?'", settings.image.execute_to_open(image_file))
+  end
+  Logger:info(string.format('Image %s.%s generated!', file, settings.image.format))
+  return string.format("sh -c '%s & disown; echo $?'", settings.image.execute_to_open(image_file))
 end
 
 local function execute_command(command, error_msg)
-    error_msg = error_msg or "Execution error!"
-    local result = vim.fn.system(command)
-    if tonumber(result) ~= 0 then
-        vim.cmd("redraw")
-        Logger:error(error_msg)
-        do return end
+  error_msg = error_msg or 'Execution error!'
+  local result = vim.fn.system(command)
+  if tonumber(result) ~= 0 then
+    vim.cmd 'redraw'
+    Logger:error(error_msg)
+    do
+      return
     end
+  end
 end
 
 local function redraw()
-    local img = string.format("%s.%s", vim.fn.expand("%:r"), settings.image.format)
-    os.execute(string.format("ps aux | grep -m 1 %s | awk '{print $2}' | xargs kill -9", img))
+  local img = string.format('%s.%s', vim.fn.expand '%:r', settings.image.format)
+  os.execute(string.format("ps aux | grep -m 1 %s | awk '{print $2}' | xargs kill -9", img))
 end
 
 function M.run()
-    if not validate() then return end
+  if not validate() then
+    return
+  end
 
-    local cli_puml = vim.fn.executable('plantuml')
-    local puml_jar = settings.puml_jar
+  local cli_puml = vim.fn.executable 'plantuml'
+  local puml_jar = settings.puml_jar
 
-    if cli_puml ~= 0 or puml_jar then
-        local file_with_extension = vim.fn.expand("%:p")
-        local file = vim.fn.expand("%:p:r")
-        local format = settings.image.format
-        local darkmode = settings.image.darkmode and "-darkmode" or ""
+  if cli_puml ~= 0 or puml_jar then
+    local file_with_extension = vim.fn.expand '%:p'
+    local file = vim.fn.expand '%:p:r'
+    print('Compiling', file)
+    local format = settings.image.format
+    local darkmode = settings.image.darkmode and '-darkmode' or ''
 
-        Logger:info("Building...")
-        if cli_puml ~= 0 then
-            local puml_command = string.format("plantuml %s -t%s %s", file_with_extension, format, darkmode)
-            if settings.actions.redraw then
-                redraw()
-            end
-            execute_command(puml_command)
-        else
-            local puml_command = string.format("java -jar %s %s -t%s %s; echo $?", puml_jar, file_with_extension, format,
-                darkmode)
-            if settings.actions.redraw then
-                redraw()
-            end
-            execute_command(puml_command)
-        end
-        execute_command(get_image_command(file), "Image not generated it.")
+    Logger:info 'Building...'
+    if cli_puml ~= 0 then
+      local puml_command = string.format('plantuml %s -t%s %s', file_with_extension, format, darkmode)
+      if settings.actions.redraw then
+        redraw()
+      end
+      execute_command(puml_command)
     else
-        Logger:warn("Install plantuml or download it from the official page and set it up with 'puml_jar' option.")
+      local puml_command = string.format('java -jar %s %s -t%s %s; echo $?', puml_jar, file_with_extension, format, darkmode)
+      if settings.actions.redraw then
+        redraw()
+      end
+      execute_command(puml_command)
     end
+    execute_command(get_image_command(file), 'Image not generated it.')
+  else
+    Logger:warn "Install plantuml or download it from the official page and set it up with 'puml_jar' option."
+  end
 end
 
 function M.open_image()
-    local file = vim.fn.expand("%:p:r")
-    execute_command(get_image_command(file), "Image not found. Run :Soil command to generate it.")
+  local file = vim.fn.expand '%:p:r'
+  execute_command(get_image_command(file), 'Image not found. Run :Soil command to generate it.')
 end
 
 return M
